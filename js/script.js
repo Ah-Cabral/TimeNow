f9ce1c03cbc186516da8245815fac56e9cf9d7a2
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-01-14 17:35:02 -0300

    :up: Adicionando Comentários no JS

--- js/script.js
+++ js/script.js
@@ -1,43 +1,58 @@
+//Utilizando função sleep
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+//Criando função changeColor, que irá trocar a cor do background e a cor da fonte de cada um
 function changeColor(cor, fontcolor){
   document.getElementById("body").style.background = cor;
   document.getElementById("clock-time").style.background = cor;
   document.getElementById("footer-font").style.color = fontcolor;
 }
 
+// O async é utilizado para permitir que uma chamada seja finalizada, pra DEPOIS continuar outra.
 async function carregar(){
+    //Cria-se um while True
     while (1) {
-        let time1 = window.document.getElementById('hour')
-        let time2 = window.document.getElementById('minute')
-        let time3 = window.document.getElementById('sec')
+
+        //Declarando variáveis referentes a cada tempo no HTML
+        let timeh = window.document.getElementById('hour')
+        let timem = window.document.getElementById('minute')
+        let times = window.document.getElementById('sec')
+
+        //E a variável referente a logo, que mudará, dependendo do horário
         let logo = window.document.getElementById('logo-header')
+
+        //Criação da variável data
         data = new Date()
+
+        //e as variáveis em JavaScript
         let hora = data.getHours()
         let minuto = data.getMinutes()
         let segundo = data.getSeconds()
 
+        //Criando condições para formatação das horas, ex: 01 -> 09
         if(hora < 10){
-            time1.innerHTML = `0${hora}`
+            timeh.innerHTML = `0${hora}`
         }else{
-            time1.innerHTML = hora
+            timeh.innerHTML = hora
         }
 
+        //Criando condições para formatação dos minutos, ex: 01 -> 09
         if(minuto < 10){
-            time2.innerHTML = `0${minuto}`
+            timem.innerHTML = `0${minuto}`
         }else{
-            time2.innerHTML = minuto
+            timem.innerHTML = minuto
         }
 
+        //Criando condições para formatação dos segundos, ex: 01 -> 09
         if(segundo < 10){
-            time3.innerHTML = `0${segundo}`
+            times.innerHTML = `0${segundo}`
         }else{
-            time3.innerHTML = segundo
+            times.innerHTML = segundo
         }
-        await sleep(1000)
 
+        //Verificação da hora para mudança do layout    
         if(hora >= 12){
             changeColor("linear-gradient(to right, #f16b11, #f5af19)", "#f16b11");
             logo.src = 'images/logo-tarde.svg'
@@ -47,6 +62,8 @@ async function carregar(){
             logo.src = 'images/logo-noite.svg'
         }
 
+        //Função sleep no final, para sempre atualizar a informação
+        await sleep(1000)
         
     }
 };
